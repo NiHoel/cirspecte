b7c75da0218216d27f0f5f18aa50384e5b18c78d
Nico Höllerich <s1nihoel@uni-bayreuth.de>
2020-06-20 18:53:56 +0200

    Add check for ES6 and display warning.

--- assets/js/init.js
+++ assets/js/init.js
@@ -1,3 +1,20 @@
+function checkES6() {
+    "use strict";
+
+    try {
+        if (typeof Symbol == "undefined")
+            throw "";
+        eval("class Foo {}");
+        eval("var bar = (x) => x+1");
+        eval("var a = async function(){}");
+        eval("let a = 5");
+    } catch (e) {
+        window.confirm("Your browser does not support ES6. Pleasue update your browser to properly display this page.");
+    }
+
+}
+checkES6();
+
 if (window.require)
     window.electron = require('electron');
 delete window.export;
